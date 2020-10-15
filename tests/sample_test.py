e022962306d9009c7061079b825b9e8589e49fd4
Vibaswan <vroychowdhury@gmail.com>
2020-10-15 14:30:15 +0530

    version ----> 1.0.4

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -18,7 +18,7 @@ def test_print_2():
 
 
 def test_docker_server():
-    url = 'http://localhost:11009/api/v1/sessions/1/ixnetwork/globals/preferences?includes=autoSaveLocation'
+    url = 'http://127.0.0.1:11009/api/v1/sessions/1/ixnetwork/globals/preferences?includes=autoSaveLocation'
     Headers = {'content-type': 'application/json'}
     response = requests.get(url=url, headers=Headers, verify={})
     print(response.json())
